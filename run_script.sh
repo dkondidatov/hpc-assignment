747836a8f6ccb203f758d2c050d8d2964e973bdd
Dmitrii Kondidatov <d.kondidatov@gmail.com>
2025-03-23 10:32:43 +0000

    Fixed slurmdbd config

--- run_script.sh
+++ run_script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH -p normal # partition (queue)
-#SBATCH --time=10
+#SBATCH --time=2
 #SBATCH --ntasks=2
 
 #SBATCH -o /mnt/fsx/slurm/slurm.%N.%j.out # STDOUT
 #SBATCH -e /mnt/fsx/slurm/slurm.%N.%j.err # STDERR
 
-srun /mnt/fsx/slurm/pmm_v2 --mpi=pmix_v5
+srun /mnt/fsx/slurm/pmm_v2
